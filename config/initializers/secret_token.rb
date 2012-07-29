b9603edbf21eec5e10af5dc33ec7225dc90f45b6
kuber <kuberaaganja@gmail.com>
2012-07-29 11:12:12 +0545

    testsss

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummyschool::Application.config.secret_token = 'ada61b3162c9005e5a367dc19209def340a19276ae630fc712d76138df4eca1de4cb32c93dd78d9214b7e135217ffdb8319c64f88c5112ce2cafc01f42e7d58f'
